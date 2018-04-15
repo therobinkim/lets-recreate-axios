355fe11b5a097294940f8af4b04417ed731bf52d
Robin Kim <therobinkim@gmail.com>
2018-04-15 13:32:47 -0500

    Add solution

--- axios.js
+++ axios.js
@@ -1,1 +1,33 @@
-const axios = {};
+const axios = {
+  get: (url) => {
+    return new Promise((resolve, reject) => {
+      $.ajax(url, {
+        method: 'GET',
+        success: (response) => {
+          resolve({
+            data: response
+          });
+        },
+        error: (error) => {
+          reject(error);
+        }
+      });
+    });
+  },
+  post: (url, data) => {
+    return new Promise((resolve, reject) => {
+      $.ajax(url, {
+        method: 'POST',
+        data,
+        success: (response) => {
+          resolve({
+            data: response
+          });
+        },
+        error: (error) => {
+          reject(error);
+        }
+      });
+    });
+  }
+};
